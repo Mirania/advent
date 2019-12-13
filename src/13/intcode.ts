22c3def6b6b56f54bade13d847622bf13c00c53c
Mirania <blakbgtr@gmail.com>
2019-12-13 17:39:31 +0000

    13

--- src/13/intcode.ts
+++ src/13/intcode.ts
@@ -6,6 +6,10 @@ export function init(code: number[]): void {
     relativebase = 0, pointerlimit = intcode.length, i = 0, halted = false;
 }
 
+export function memset(address: number, value: number): void {
+    intcode[address] = value;
+}
+
 export function done(): boolean {
     return halted;
 }
@@ -17,14 +21,14 @@ export function cpu(input?: number): [number, number, number] {
         switch (intcode[i] % 100) { //last 1 or 2 digits of instruction
             case 1: intcode[write(i, 3)] = read(i, 1) + read(i, 2); i += 4; break;
             case 2: intcode[write(i, 3)] = read(i, 1) * read(i, 2); i += 4; break;
-            case 3: if (!input) throw new Error("no input"); intcode[write(i, 1)] = input; i += 2; break;
+            case 3: if (input===undefined) throw new Error("no input"); intcode[write(i, 1)] = input; i += 2; break;
             case 4: outputs.push(read(i, 1)); i += 2; if (outputs.length===3) return outputs as any; break;
             case 5: i = read(i, 1) !== 0 ? read(i, 2) : i + 3; break;
             case 6: i = read(i, 1) === 0 ? read(i, 2) : i + 3; break;
             case 7: intcode[write(i, 3)] = read(i, 1) < read(i, 2) ? 1 : 0; i += 4; break;
             case 8: intcode[write(i, 3)] = read(i, 1) === read(i, 2) ? 1 : 0; i += 4; break;
             case 9: relativebase += read(i, 1); i += 2; break;
-            case 99: halted = true; return;
+            case 99: halted = true; return [,,,];
         }
     }
 }
--- src/13/main.ts
+++ src/13/main.ts
@@ -1,27 +1,40 @@
 import * as fs from 'fs';
 import * as program from './intcode';
 
-type Tile = { color: number, point?: Point };
 type Point = { x: number, y: number };
 
 fs.readFile("src/13/input.txt", { encoding: 'utf8' }, (err, text) => {
     if (err) { console.log(err); return; }
 
-    let originalcode = text.split(",").map((value) => parseInt(value));
-    
-    program.init(originalcode); let tiles = {};
-    let counter = 0;
-    while (true) {
-       let outputs = program.cpu(); if (program.done()) break; 
-       if (outputs[2]===2) counter++;
-    }
+    let intcode = text.split(",").map((value) => parseInt(value));
 
-    console.log(counter);
-
+    let metrics = stats(intcode);
+    console.log("Part 1:", metrics.blocks);
+    console.log("Part 2:", game(intcode, metrics.max, true));
 });
 
-function draw(painted: { [xy: string]: Tile }, min: Point, max: Point): string {
-    let canvas: string[][] = Array(max.y-min.y+1).fill("").map(() => Array(max.x-min.x+1).fill(" "));
-    Object.keys(painted).forEach(tile => canvas[painted[tile].point.y-min.y][painted[tile].point.x-min.x] = painted[tile].color===0 ? " " : "█");
-    return canvas.map(line => `\n${line.join("")}`).join("");
+function stats(intcode: number[]): { blocks: number, max: Point } {
+    program.init(intcode);
+    let counter = 0, max = {x: -Infinity, y: -Infinity}, x: number, y: number, type: number;
+    while (true) { 
+        [x, y, type] = program.cpu(); if (program.done()) return {blocks: counter, max: max}; 
+        if (x > max.x) max.x = x; if (y > max.y) max.y = y; if (type === 2) counter++;   
+    }
+}
+
+function game(intcode: number[], max: Point, headless: boolean): number {
+    program.init(intcode); program.memset(0x0, 2);
+    let board: string[][] = Array(max.y+1).fill("").map(() => Array(max.x+1).fill(" ")), input = 0, score = 0, ball: Point, pad: Point;
+    while (true) { 
+        let output = program.cpu(input); if (program.done()) return score;
+        if (output[2]===4) ball = {x: output[0], y: output[1]}; if (output[2]===3) pad = {x: output[0], y: output[1]}; if (output[0]===-1) score = output[2];
+        if (ball && pad) input = ball.x<pad.x ? -1 : ball.x>pad.x ? 1 : 0; //insane neural network
+        if (!headless) draw(board, output); 
+    }
+}
+
+function draw(board: string[][], tile: [number, number, number]): void {
+    let empty = " ", wall = "█", block = "◻", paddle = "▬", ball = "●";
+    board[tile[1]][tile[0]] = tile[2]===0 ? empty : tile[2]===1 ? wall : tile[2]===2 ? block : tile[2]===3 ? paddle : ball;
+    console.log(board.map(line => `\n${line.join("")}`).join(""));
 }
